9808b656bfc01928334159d7d2afb599de3e49a5
Franco Cedillo <franco.cedillo@gmail.com>
2014-12-15 15:58:53 -0500

    adds support for squares with numbers

--- app/scripts/crossword.js
+++ app/scripts/crossword.js
@@ -9,25 +9,45 @@
     var currOri = 'across';
     var currCol = 0;
     var next = 39;
-    var gotMatrix = true;
-    var matrix =
-[
- [ 1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  0,  1,  1,  1,  1],
- [ 1,  0,  1,  0,  1,  0,  1,  0,  1,  0,  1,  0,  1,  0,  1],
- [ 1,  1,  1,  0,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1],
- [ 1,  0,  1,  0,  1,  0,  1,  0,  1,  0,  1,  0,  1,  0,  1],
- [ 1,  1,  1,  1,  1,  0,  1,  1,  1,  1,  1,  1,  1,  1,  0],
- [ 1,  0,  1,  0,  1,  0,  0,  0,  1,  0,  1,  0,  1,  0,  0],
- [ 1,  1,  1,  1,  1,  1,  1,  0,  1,  0,  0,  1,  1,  1,  1],
- [ 1,  0,  0,  0,  1,  0,  1,  0,  1,  0,  1,  0,  0,  0,  1],
- [ 1,  1,  1,  1,  0,  0,  1,  0,  1,  1,  1,  1,  1,  1,  1],
- [ 0,  0,  1,  0,  1,  0,  1,  0,  0,  0,  1,  0,  1,  0,  1],
- [ 0,  1,  1,  1,  1,  1,  1,  1,  1,  0,  1,  1,  1,  1,  1],
- [ 1,  0,  1,  0,  1,  0,  1,  0,  1,  0,  1,  0,  1,  0,  1],
- [ 1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  0,  1,  1,  1],
- [ 1,  0,  1,  0,  1,  0,  1,  0,  1,  0,  1,  0,  1,  0,  1],
- [ 1,  1,  1,  1,  0,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1]
-];
+    var gotSquareMatrix = true;
+    var gotNumMatrix = true;
+    var squareMatrix =
+    [
+     [ 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1],
+     [ 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1],
+     [ 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+     [ 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1],
+     [ 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1],
+     [ 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1],
+     [ 0, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 0, 0, 1],
+     [ 1, 0, 1, 0, 0, 0, 1, 0, 1, 0, 0, 0, 1, 0, 1],
+     [ 1, 0, 0, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 0],
+     [ 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1],
+     [ 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1],
+     [ 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1],
+     [ 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1],
+     [ 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1],
+     [ 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+    ];
+
+    var numMatrix =
+    [
+     [  1,  0,  2,  0,  3,  0,  4,  0,  5,  0,  0,  6,  7,  0,  8],
+     [  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  9,  0,  0,  0,  0],
+     [ 10,  0,  0,  0,  0,  0, 11,  0,  0,  0,  0,  0,  0,  0,  0],
+     [  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0],
+     [ 12,  0,  0,  0,  0,  0,  0, 13,  0,  0,  0,  0,  0,  0,  0],
+     [  0,  0,  0,  0,  0,  0, 14,  0,  0,  0,  0,  0,  0,  0,  0],
+     [  0, 15,  0,  0,  0,  0,  0,  0,  0, 16,  0,  0,  0,  0,  0],
+     [ 17,  0,  0,  0,  0,  0,  0,  0, 18,  0,  0,  0, 19,  0,  0],
+     [  0,  0,  0, 20, 21,  0,  0,  0, 22,  0, 23,  0,  0,  0,  0],
+     [  0,  0, 24,  0,  0,  0, 25,  0,  0,  0,  0,  0,  0,  0, 26],
+     [ 27,  0,  0,  0,  0,  0,  0,  0,  0, 28,  0,  0,  0,  0,  0],
+     [  0,  0,  0,  0,  0,  0,  0,  0, 29,  0,  0,  0,  0,  0,  0],
+     [ 30,  0,  0,  0,  0,  0,  0,  0,  0,  0, 31,  0,  0,  0,  0],
+     [  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0],
+     [ 32,  0,  0,  0,  0, 33,  0,  0,  0,  0,  0,  0,  0,  0,  0]
+    ];
 
     var puzInit = {
 
@@ -107,26 +127,49 @@
 
         var isChecked;
         
-        if (gotMatrix) {
-
-          console.log('gotMatrix');
+        if (gotSquareMatrix) {
 
           for (var i=1; i <= rows; ++i) {
             tbl.push("<tr>");
             for (var j=1; j <= cols; ++j) {
-              isChecked = matrix[i-1][j-1];
-              if ( isChecked == 0) {
-                tbl.push('<td data-coords="' + i + ',' + j + '"><span></span><input class="checked" maxlength="1" val="" type="text" onkeydown="return alphaOnly(event);" /></td>');
+              isChecked = squareMatrix[i-1][j-1];
+              
+              if (gotNumMatrix) {
+
+                squareNum = numMatrix[i-1][j-1]
+                if (squareNum == 0) {
+
+                  if ( isChecked == 0) {
+                    tbl.push('<td data-coords="' + i + ',' + j + '"><span></span><input class="checked" maxlength="1" val="" type="text" onkeydown="return alphaOnly(event);" /></td>');
+                  } else {
+                    tbl.push('<td data-coords="' + i + ',' + j + '"><span></span><input maxlength="1" val="" type="text" onkeydown="return alphaOnly(event);" /></td>');
+                  };
+
+                } else {
+
+                  if ( isChecked == 0) {
+                    tbl.push('<td data-coords="' + i + ',' + j + '"><span></span><input class="checked" maxlength="1" val="" type="text" onkeydown="return alphaOnly(event);" /></td>');
+                  } else {
+                    tbl.push('<td data-coords="' + i + ',' + j + '"><span>' + squareNum + '</span><input maxlength="1" val="" type="text" onkeydown="return alphaOnly(event);" /></td>');
+                  };
+
+                }
+
               } else {
-                tbl.push('<td data-coords="' + i + ',' + j + '"><span></span><input maxlength="1" val="" type="text" onkeydown="return alphaOnly(event);" /></td>');
+
+                if ( isChecked == 0) {
+                  tbl.push('<td data-coords="' + i + ',' + j + '"><span></span><input class="checked" maxlength="1" val="" type="text" onkeydown="return alphaOnly(event);" /></td>');
+                } else {
+                  tbl.push('<td data-coords="' + i + ',' + j + '"><span></span><input maxlength="1" val="" type="text" onkeydown="return alphaOnly(event);" /></td>');
+                };
+
               };
+
             }
             tbl.push("</tr>");
           };
 
         } else {
-
-          console.log('No Matrix');
 
           for (var i=1; i <= rows; ++i) {
             tbl.push("<tr>");
